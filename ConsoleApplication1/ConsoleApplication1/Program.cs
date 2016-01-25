a1b92ffd41d480ac1e532aaff577461d1858e6ec
kowal <pkowalski91@gmail.com>
2016-01-25 20:43:43 +0100

    first

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp_Samples
+{
+    public class SemaphoreSample
+    {
+        // tworzy objekt semaphore, tylko 3 miejsca dostepne
+
+        static Semaphore semaphoreObject = new Semaphore(3, 4);
+
+        private static void DoWork()
+        {
+            Console.WriteLine("{0} czeka w kolejce...", Thread.CurrentThread.Name);
+
+            // watek czeka na dostepne miejsce
+            semaphoreObject.WaitOne();
+
+            Console.WriteLine("{0} wchodzi w krytyczna sciezke!", Thread.CurrentThread.Name);
+
+            // symuluje prace przez czekanie 1 sek
+            Thread.Sleep(1000);
+
+            Console.WriteLine("{0} opuszcza krytyczna sciezke", Thread.CurrentThread.Name);
+
+            // wykonuje miejsce
+            semaphoreObject.Release();
+        }
+
+        public static void Main()
+        {
+            // tworzy 10 watkow, nazywa
+            // startuje prace na watkach
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(DoWork);
+                thread.Name = "Thread_" + i;
+                thread.Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
